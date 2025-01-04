4502b891f592dc6be53ab9db06119e9ef64b35dd
lakshyaag <lakshyajannu@gmail.com>
2025-01-04 15:51:28 -0500

    init: ios app

---
+++ recipe-view/recipe-view/recipe_viewApp.swift
@@ -0,0 +1,17 @@
+//
+//  recipe_viewApp.swift
+//  recipe-view
+//
+//  Created by Lakshya Agarwal on 2025-01-04.
+//
+
+import SwiftUI
+
+@main
+struct recipe_viewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
