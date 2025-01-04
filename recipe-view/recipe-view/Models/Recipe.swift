a2effd794b2d48eaf3871672978d1843ca5642a5
lakshyaag <lakshyajannu@gmail.com>
2025-01-04 17:14:18 -0500

    cr

--- recipe-view/recipe-view/Models/Recipe.swift
+++ recipe-view/recipe-view/Models/Recipe.swift
@@ -1,16 +1,11 @@
 import Foundation
 
-struct Recipe: Identifiable, Decodable {
-    let id = UUID()
+struct Recipe: Decodable {
+    
     let title: String
     let ingredients: [String]
     let instructions: [String]
-    
-    private enum CodingKeys: String, CodingKey {
-        case title
-        case ingredients
-        case instructions
-    }
+	
     
     static let mockRecipe = Recipe(
         title: "Delicious Pasta Carbonara",
