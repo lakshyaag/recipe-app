128ae7e16299cc14c9932a025df91de0dfbdf386
lakshyaag <lakshyajannu@gmail.com>
2025-01-04 22:41:41 -0500

    feat: Refactor recipe processing workflow to include link parsing step

--- python-backend/src/graph.py
+++ python-backend/src/graph.py
@@ -1,27 +1,50 @@
 from langgraph.graph import StateGraph, END, START
 
-from src.utils.document_loader import get_recipe_from_url
+from src.utils.document_loader import get_website_data
 from src.tools.recipe_tool import Recipe, format_recipe
 from src.schemas.state import GraphState
 
 
-def process_recipe(state: GraphState) -> Recipe:
+def parse_link(state: GraphState) -> GraphState:
     try:
         url = state["url"]
-        print(url)
-        website_data = get_recipe_from_url(url)
-        print(website_data)
+        website_data = get_website_data(url)
+
+        return {"website_data": website_data}
+
+    except Exception as e:
+        raise e
+
+
+def process_recipe(state: GraphState) -> Recipe:
+    website_data = state["website_data"]
+    try:
+        if not website_data:
+            raise ValueError("No website data found")
 
         recipe = format_recipe(website_data)
         return {"recipe": recipe}
+
+    except Exception as e:
+        raise e
+
+
+def extract_times(state: GraphState) -> GraphState:
+    recipe = state["recipe"]
+    try:
+        if not recipe:
+            raise ValueError("No recipe found")
+
     except Exception as e:
         raise e
 
 
 workflow = StateGraph(GraphState)
+workflow.add_node("parse_link", parse_link)
 workflow.add_node("process_recipe", process_recipe)
-workflow.add_edge(START, "process_recipe")
-workflow.set_entry_point("process_recipe")
+workflow.add_edge(START, "parse_link")
+workflow.add_edge("parse_link", "process_recipe")
 workflow.add_edge("process_recipe", END)
+workflow.set_entry_point("parse_link")
 
 graph = workflow.compile()
--- python-backend/src/tools/recipe_tool.py
+++ python-backend/src/tools/recipe_tool.py
@@ -1,3 +1,4 @@
+from typing import List
 from langchain_core.messages import SystemMessage, HumanMessage
 from langchain_openai import ChatOpenAI
 from langchain_core.documents import Document
@@ -5,13 +6,13 @@ from src.schemas.recipe import Recipe
 from src.utils.text_utils import sanitize_text
 
 
-def format_recipe(website_data: Document) -> Recipe:
+def format_recipe(website_data: List[Document]) -> Recipe:
     model = ChatOpenAI(model="gpt-4o-mini", temperature=0)
     messages = [
         SystemMessage(
             "Please extract recipes from the following website content. If a recipe is not found, return an empty recipe."
         ),
-        HumanMessage(content=sanitize_text(website_data.page_content)),
+        HumanMessage(content=sanitize_text(website_data[0].page_content)),
     ]
 
     response = model.with_structured_output(Recipe).invoke(messages)
